9ad1f4f13be062103390846351a01beacb0d97c0
Michael Zdravkovski <mzdravkovski@cocoondata.com>
2023-04-22 11:53:37 +0800

    Update template folder

--- web/__init__.py
+++ web/__init__.py
@@ -5,11 +5,13 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, template_folder='./Templates')
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'web.sqlite'),
     )
+
+
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
