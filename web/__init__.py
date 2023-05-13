cfb2462b6bc9782b33323bbcbc025049da136792
Michael Zdravkovski <mzdravkovski@cocoondata.com>
2023-05-13 18:56:30 +0800

    Fix test error

--- web/__init__.py
+++ web/__init__.py
@@ -39,7 +39,8 @@ def create_app(test_config=None):
 
     from . import db
     db.init_app(app)
-    mail.init_app(app)
+    mail = Mail(app)
+    # mail.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
