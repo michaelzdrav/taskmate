a5826a95bf447cc83f039cd21a38c763457848c6
harry <harrisongitau3@gmail.com>
2023-06-06 23:05:54 +0300

    Fix: Update naming conventions as required by sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,22 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from config import Config
+from sqlalchemy import MetaData
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+metadata = MetaData(naming_convention=convention)
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(app, metadata=metadata)
 migrate = Migrate(app, db, render_as_batch=True)
 moment = Moment(app)
 login = LoginManager(app)
