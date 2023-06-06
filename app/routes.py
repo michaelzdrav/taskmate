d35c048428d365dcce87063dcbd22c2d12d3dc07
harry <harrisongitau3@gmail.com>
2023-06-06 21:30:59 +0300

    Fix: Redirect user once not found

--- app/routes.py
+++ app/routes.py
@@ -31,6 +31,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password.')
+            return redirect(url_for('login'))
         next_page = request.args.get("next")
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
